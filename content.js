73366f11582dc4ecded41a01cd9f2eac25d1c619
Sawyer Hollenshead <git@sawyerh.com>
2020-11-19 08:49:35 -0500

    Format files

--- content.js
+++ content.js
@@ -1,2 +1,143 @@
-
-content = [{                    "start": 0,                "image": "/assets/PappyGma-small.jpg"    },{                    "start": 8000,                "image": "/assets/ward-reunion.jpg"    },{                    "start": 46000,                  "lat": 39.829125,        "long": -77.905285,        "pitch": -3,        "heading": 15.32,            "image": "/assets/fries-funeral-home.jpg"    },{                    "start": 100000,                "image": "/assets/ward-family.jpg"    },{                    "start": 120000,                  "lat": 39.830699,        "long": -77.903905,        "pitch": 3.82,        "heading": 73.57,            "image": "/assets/rues-bar.jpg"    },{                    "start": 140000,                "image": "/assets/pappy-young.jpg"    },{                    "start": 148000,                "image": "/assets/barn.jpg"    },{                    "start": 174000,          "video": "/assets/Cow and calf - optimized.mp4",      "ogv": "/assets/Cow and calf - optimized.oggtheora.ogv",      "youtube": "C0BnI-D7yJk",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc8d5dde2283a10000d4.jpg"    },{                    "start": 186000,                "image": "/assets/scan0056.jpg"    },{                    "start": 222000,          "video": "/assets/Cows and storm - optimized.mp4",      "ogv": "/assets/Cows and storm - optimized.oggtheora.ogv",      "youtube": "rlbTguItgZ0",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc8d5dde22956f000180.jpg"    },{                    "start": 250000,                "image": "/assets/Old Barns.jpg"    },{                    "start": 304000,          "video": "/assets/Haybales on tractor.mp4",      "ogv": "/assets/Haybales on tractor - optimized.oggtheora.ogv",      "youtube": "blN4ONPA4HE",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc905dde22956f000181.jpg"    },{                    "start": 329000,                "image": "/assets/gparents.jpg"    },{                    "start": 361000,                "image": "/assets/Barn2.jpg"    },{                    "start": 378000,          "video": "/assets/Turkey walking - optimized.mp4",      "ogv": "/assets/Turkey walking - optimized.oggtheora.ogv",      "youtube": "fD8ug6oEwnM",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc945dde22956f000182.jpg"    },{                    "start": 420000,          "video": "/assets/Turkies and Dad - optimized.mp4",      "ogv": "/assets/Turkies and Dad - optimized.oggtheora.ogv",      "youtube": "SqWfWWxbQVU",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc945dde2283a10000d5.jpg"    },{                    "start": 464000,                "image": "/assets/DadBaby.jpg"    },{                    "start": 515000,                "image": "/assets/DadPig.jpg"    },{                    "start": 580000,          "video": "/assets/Pigs - optimized.mp4",      "ogv": "/assets/Pigs - optimized.oggtheora.ogv",      "youtube": "9IeDAqRgjg4",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc9f5dde22cf3800009c.jpg"    },{                    "start": 646000,          "video": "/assets/Fields panorama.mp4",      "ogv": "/assets/Fields panorama.oggtheora.ogv",      "youtube": "7RLGUIdo0GM",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde2283a10000d2.jpg"    },{                    "start": 694000,          "video": "/assets/Corn stacks in field_1.mp4",      "ogv": null,      "youtube": "Hv5oZKkVfPs",      "image": "/assets/Corn-stacks-in-field.jpg"    },{                    "start": 722000,          "video": "/assets/Sheila and dad.mp4",      "ogv": "/assets/Sheila and dad.oggtheora.ogv",      "youtube": "Mrnk7RQDwHo",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde2283a10000d3.jpg"    },{                    "start": 755000,          "video": "/assets/Pepper climbing fence_1.mp4",      "ogv": "/assets/Pepper climbing fence_1.oggtheora.ogv",      "youtube": "hV6EWwQDfx4",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde22159a00027f.jpg"    },{                    "start": 777000,                "image": "/assets/GmaMiddle.jpg"    },{                    "start": 939000,                "image": "/assets/gmaonHorse.jpg"    },{                    "start": 1004000,                "image": "/assets/GmaYoung.jpg"    },{                    "start": 1071000,                "image": "/assets/scan0033.jpg"    },{                    "start": 1124000,                "image": "/assets/GmaYounger.jpg"    },{                    "start": 1209000,                "image": "/assets/PappyHunting.jpg"    },{                    "start": 1240000,                "image": "/assets/warren_news_2.jpg"    },{                    "start": 1265000,                "image": "/assets/GmaFam.jpg"    },{                    "start": 1291000,                "image": "/assets/Pappy.jpg"    },{                    "start": 1335000,          "video": "/assets/Gma shoveling snow - optimized.mp4",      "ogv": "/assets/Gma shoveling snow - optimized.oggtheora.ogv",      "youtube": "hBkPHS7IirE",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3da565dde22956f00017c.jpg"    },{                    "start": 1351000,          "video": "/assets/Red car.mp4",      "ogv": "/assets/Red car.oggtheora.ogv",      "youtube": "RufCH12Jtv4",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3d6005dde22159a00027c.png"    },{                    "start": 1404000,                "image": "/assets/PappyRebel.jpg"    },{                    "start": 1437000,                "image": "/assets/PappyRight.jpg"    },{                    "start": 1506000,          "video": "/assets/Sunset.mp4",      "ogv": "/assets/Sunset.oggtheora.ogv",      "youtube": "2FsgLyZSnOg",      "image": "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3d9515dde22cf38000099.png"    }];
+content = [
+  { start: 0, image: "/assets/PappyGma-small.jpg" },
+  { start: 8000, image: "/assets/ward-reunion.jpg" },
+  {
+    start: 46000,
+    lat: 39.829125,
+    long: -77.905285,
+    pitch: -3,
+    heading: 15.32,
+    image: "/assets/fries-funeral-home.jpg",
+  },
+  { start: 100000, image: "/assets/ward-family.jpg" },
+  {
+    start: 120000,
+    lat: 39.830699,
+    long: -77.903905,
+    pitch: 3.82,
+    heading: 73.57,
+    image: "/assets/rues-bar.jpg",
+  },
+  { start: 140000, image: "/assets/pappy-young.jpg" },
+  { start: 148000, image: "/assets/barn.jpg" },
+  {
+    start: 174000,
+    video: "/assets/Cow and calf - optimized.mp4",
+    ogv: "/assets/Cow and calf - optimized.oggtheora.ogv",
+    youtube: "C0BnI-D7yJk",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc8d5dde2283a10000d4.jpg",
+  },
+  { start: 186000, image: "/assets/scan0056.jpg" },
+  {
+    start: 222000,
+    video: "/assets/Cows and storm - optimized.mp4",
+    ogv: "/assets/Cows and storm - optimized.oggtheora.ogv",
+    youtube: "rlbTguItgZ0",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc8d5dde22956f000180.jpg",
+  },
+  { start: 250000, image: "/assets/Old Barns.jpg" },
+  {
+    start: 304000,
+    video: "/assets/Haybales on tractor.mp4",
+    ogv: "/assets/Haybales on tractor - optimized.oggtheora.ogv",
+    youtube: "blN4ONPA4HE",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc905dde22956f000181.jpg",
+  },
+  { start: 329000, image: "/assets/gparents.jpg" },
+  { start: 361000, image: "/assets/Barn2.jpg" },
+  {
+    start: 378000,
+    video: "/assets/Turkey walking - optimized.mp4",
+    ogv: "/assets/Turkey walking - optimized.oggtheora.ogv",
+    youtube: "fD8ug6oEwnM",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc945dde22956f000182.jpg",
+  },
+  {
+    start: 420000,
+    video: "/assets/Turkies and Dad - optimized.mp4",
+    ogv: "/assets/Turkies and Dad - optimized.oggtheora.ogv",
+    youtube: "SqWfWWxbQVU",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc945dde2283a10000d5.jpg",
+  },
+  { start: 464000, image: "/assets/DadBaby.jpg" },
+  { start: 515000, image: "/assets/DadPig.jpg" },
+  {
+    start: 580000,
+    video: "/assets/Pigs - optimized.mp4",
+    ogv: "/assets/Pigs - optimized.oggtheora.ogv",
+    youtube: "9IeDAqRgjg4",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3dc9f5dde22cf3800009c.jpg",
+  },
+  {
+    start: 646000,
+    video: "/assets/Fields panorama.mp4",
+    ogv: "/assets/Fields panorama.oggtheora.ogv",
+    youtube: "7RLGUIdo0GM",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde2283a10000d2.jpg",
+  },
+  {
+    start: 694000,
+    video: "/assets/Corn stacks in field_1.mp4",
+    ogv: null,
+    youtube: "Hv5oZKkVfPs",
+    image: "/assets/Corn-stacks-in-field.jpg",
+  },
+  {
+    start: 722000,
+    video: "/assets/Sheila and dad.mp4",
+    ogv: "/assets/Sheila and dad.oggtheora.ogv",
+    youtube: "Mrnk7RQDwHo",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde2283a10000d3.jpg",
+  },
+  {
+    start: 755000,
+    video: "/assets/Pepper climbing fence_1.mp4",
+    ogv: "/assets/Pepper climbing fence_1.oggtheora.ogv",
+    youtube: "hV6EWwQDfx4",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3db4a5dde22159a00027f.jpg",
+  },
+  { start: 777000, image: "/assets/GmaMiddle.jpg" },
+  { start: 939000, image: "/assets/gmaonHorse.jpg" },
+  { start: 1004000, image: "/assets/GmaYoung.jpg" },
+  { start: 1071000, image: "/assets/scan0033.jpg" },
+  { start: 1124000, image: "/assets/GmaYounger.jpg" },
+  { start: 1209000, image: "/assets/PappyHunting.jpg" },
+  { start: 1240000, image: "/assets/warren_news_2.jpg" },
+  { start: 1265000, image: "/assets/GmaFam.jpg" },
+  { start: 1291000, image: "/assets/Pappy.jpg" },
+  {
+    start: 1335000,
+    video: "/assets/Gma shoveling snow - optimized.mp4",
+    ogv: "/assets/Gma shoveling snow - optimized.oggtheora.ogv",
+    youtube: "hBkPHS7IirE",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3da565dde22956f00017c.jpg",
+  },
+  {
+    start: 1351000,
+    video: "/assets/Red car.mp4",
+    ogv: "/assets/Red car.oggtheora.ogv",
+    youtube: "RufCH12Jtv4",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3d6005dde22159a00027c.png",
+  },
+  { start: 1404000, image: "/assets/PappyRebel.jpg" },
+  { start: 1437000, image: "/assets/PappyRight.jpg" },
+  {
+    start: 1506000,
+    video: "/assets/Sunset.mp4",
+    ogv: "/assets/Sunset.oggtheora.ogv",
+    youtube: "2FsgLyZSnOg",
+    image:
+      "https://siteleaf-cdn.s3.amazonaws.com/53d16d105dde2275fb0001ab/assets/53d3d9515dde22cf38000099.png",
+  },
+];
